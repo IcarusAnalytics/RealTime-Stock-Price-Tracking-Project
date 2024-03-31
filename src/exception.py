114208573a5b6d6b0392965812da68fa9d08f281
unknown <analyticsicarus@gmail.com>
2024-03-31 13:21:28 +0300

    'setup'

--- src/exception.py
+++ src/exception.py
@@ -24,13 +24,13 @@ class CustomException(Exception):
 
 
 # Use for Testing
-if __name__=="__main__":
-    try:
-        a=1/0
+#if __name__=="__main__":
+#    try:
+#        a=1/0
 
-    except Exception as e:
-        logging.info("Divide by Zero Error")
-        raise CustomException(e,sys)
+#    except Exception as e:
+#        logging.info("Divide by Zero Error")
+#        raise CustomException(e,sys)
     
 
 
